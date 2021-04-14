4f23b69e62b5edf1a71edc4cd9272d67c6d4f18b
Mxx1029 <lia.pim@icloud.com>
2021-04-14 16:06:46 +0200

    Add bonus challenge

--- start.js
+++ start.js
@@ -91,4 +91,34 @@ const diff = Math.abs(specNumber - 19);
 
 // 11. BONUS CHALLENGE:  Make a variable for firstName and age and give each variable values. Create an if/else statement to check whether the person's age is less than 13. If so, print "firstName is a child". If the age is equal to or more than 13 and less than 20, print "firstName is a teenager". If the person's age is equal to and more than 20 and less than 30, then print "firstName is a young adult". If none of these conditions apply, print "firstName is a adult". 
 
+const firstName = "Angela";
+const age = 64;
+
+if (age < 13) {
+    console.log(`${firstName} is a child.`);
+} else if (age >= 13 && age < 20) {
+    console.log(`${firstName} is a teenager.`);
+} else if (age >= 20 && age < 30) {
+    console.log(`${firstName} is a young adult.`);
+} else {
+    console.log(`${firstName} is an adult.`);
+};
+
+// OR:
+
+switch (age) {
+    case (age < 13) :
+        console.log(`${firstName} is a child.`);
+        break;
+    case (age >= 13 && age < 20) :
+        console.log(`${firstName} is a teenager.`);
+        break;
+    case (age >= 20 && age < 30) :
+        console.log(`${firstName} is a young adult.`);
+        break;
+    default :
+        console.log(`${firstName} is an adult.`);
+        break;
+};
+
 // 12. BONUS CHALLENGE: Play around with each of your results, i.e., make sure they are dynamic to different inputs; is there a more efficient way of writing your code?; etc. 
